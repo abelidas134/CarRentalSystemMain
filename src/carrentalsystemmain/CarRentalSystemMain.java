4e78f0b6145a30626015b7797ab9738e8c8f36a6
Mickey <abelidasmickey134@gmail.com>
2026-03-18 16:28:42 +0800

    Initial commit

---
+++ src/carrentalsystemmain/CarRentalSystemMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package carrentalsystemmain;
+
+/**
+ *
+ * @author Mickey
+ */
+public class CarRentalSystemMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //All proj
+    }
+    
+}
