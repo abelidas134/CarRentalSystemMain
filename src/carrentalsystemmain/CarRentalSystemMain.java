28f8c16814324d7e66415d7681b01b9f8776b144
Judeah Jamsy N. Maldecir <judeahjamsymaldecir@gmail.com>
2026-04-12 21:09:00 +0800

    Judeah; Moved my classes & main class

--- src/carrentalsystemmain/CarRentalSystemMain.java
+++ src/carrentalsystemmain/CarRentalSystemMain.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package carrentalsystemmain;
 
-/**
- *
- * @author Mickey
- */
 public class CarRentalSystemMain {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        //All proj
+  
+   public static void main(String[] args) {
+        staff_LogInPage slp = new staff_LogInPage();
+        slp.setVisible(true);
+
+//            carMenu cm = new carMenu();
+//            cm.setVisible(true);
+
+//               carStatusToyotaVios1270 csToyota1270 = new carStatusToyotaVios1270();
+//               csToyota1270.setVisible(true);
+
     }
+
     
 }
