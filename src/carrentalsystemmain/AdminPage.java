4fd4608e5384661441c2f50ef13bb1de940a0ea9
Mickey <abelidasmickey134@gmail.com>
2026-04-17 15:19:17 +0800

    Main Pages

--- src/carrentalsystemmain/AdminPage.java
+++ src/carrentalsystemmain/AdminPage.java
@@ -9,6 +9,9 @@ import javax.swing.*;
  * @author Mickey
  */
 class AdminPage extends JFrame implements ActionListener {
+    private JLabel lblMessage, lblUserName, lblPassword;
+    private JTextField txtUserName, txtPassword;
+    private JButton btnLogin, btnBack;
     
     AdminPage(){
         setSize(600,600);
@@ -17,12 +20,52 @@ class AdminPage extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        lblMessage = new JLabel ("ADMIN ACCOUNT");
+        lblMessage.setBounds(250, 125, 500,100);
+        add(lblMessage);
         
+        lblUserName = new JLabel ("Username:");
+        lblUserName.setBounds(190,175, 100,100);
+        add(lblUserName);
+        
+        txtUserName = new JTextField ();
+        txtUserName.setBounds(290,210,150,30);
+        add(txtUserName);
+        
+        lblPassword = new JLabel ("Password:");
+        lblPassword.setBounds(190,215,100,100);
+        add(lblPassword);
+        
+        txtPassword = new JTextField ();
+        txtPassword.setBounds(290,250,150,30);
+        add(txtPassword);
+        
+        btnLogin = new JButton ("Log In");
+        btnLogin.setBounds(240,300,100,25);
+        add(btnLogin);
+        
+        btnBack = new JButton ("Back");
+        btnBack.setBounds(10,520,100,25);
+        add(btnBack);
+        
+        btnLogin.addActionListener(this);
+        btnBack.addActionListener(this);
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource()==btnLogin){
+            String userName = txtUserName.getText().trim();
+            String pass = txtPassword.getText().trim();
+            if (userName.matches("admin123")&&pass.matches("0000")){
+                //customer
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid account!","ERROR",JOptionPane.ERROR_MESSAGE);
+            }
+        } else if (e.getSource()==btnBack){
+            Homepage hp = new Homepage();
+            hp.setVisible(true);
+        }
     }
     
 }
--- src/carrentalsystemmain/CustomerPage.java
+++ src/carrentalsystemmain/CustomerPage.java
@@ -4,14 +4,74 @@
  */
 package carrentalsystemmain;
 
+import java.awt.event.*;
+import javax.swing.*;
+
 /**
  *
  * @author Mickey
  */
-class CustomerPage {
+class CustomerPage extends JFrame implements ActionListener {
+    private JLabel lblMessage, lblUserName, lblPassword;
+    private JTextField txtUserName, txtPassword;
+    private JButton btnLogin, btnBack;
+    
+    CustomerPage(){
+        setSize(600,600);
+        setTitle("Customer Account");
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        lblMessage = new JLabel ("CUSTOMER ACCOUNT");
+        lblMessage.setBounds(230, 125, 500,100);
+        add(lblMessage);
+        
+        lblMessage = new JLabel ("Check your receipt and login your account!");
+        lblMessage.setBounds(180, 150, 500,100);
+        add(lblMessage);
+        
+        lblUserName = new JLabel ("Username:");
+        lblUserName.setBounds(175,185, 100,100);
+        add(lblUserName);
+        
+        txtUserName = new JTextField ();
+        txtUserName.setBounds(275,220,150,30);
+        add(txtUserName);
+        
+        lblPassword = new JLabel ("Password:");
+        lblPassword.setBounds(175,225,100,100);
+        add(lblPassword);
+        
+        txtPassword = new JTextField ();
+        txtPassword.setBounds(275,260,150,30);
+        add(txtPassword);
+        
+        btnLogin = new JButton ("Log In");
+        btnLogin.setBounds(250,310,100,25);
+        add(btnLogin);
+        
+        btnBack = new JButton ("Back");
+        btnBack.setBounds(10,520,100,25);
+        add(btnBack);
+        
+        btnLogin.addActionListener(this);
+        btnBack.addActionListener(this);
+        
+    }
 
-    void setVisible(boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==btnLogin){
+            String userName = txtUserName.getText().trim();
+            String pass = txtPassword.getText().trim();
+            if(userName.matches("user123")&&pass.matches("0000")){
+                //C
+            }
+        } else if (e.getSource()==btnBack){
+            Homepage hp = new Homepage();
+            hp.setVisible(true);
+        }
     }
     
 }
