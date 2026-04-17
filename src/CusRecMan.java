8fc1b23a713a3ae2b9ba95f0331aa30997a92c7b
Mickey <abelidasmickey134@gmail.com>
2026-04-17 15:34:18 +0800

    Modified

--- src/CusRecMan.java
+++
@@ -1,8 +0,0 @@
-package CusRecMan;
-
-public class CusRecMan {
-    public static void main(String[] args) {
-        HomePage hp = new HomePage();
-        hp.setVisible(true);
-    }
-}
--- src/Searchable.java
+++
@@ -1,5 +0,0 @@
-package CusRecMan;
-
-public interface Searchable {
-    void search(String keyword);
-}
